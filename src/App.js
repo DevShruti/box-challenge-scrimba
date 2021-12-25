0731a51fd33cefb90dcc644b526f84aa7a775ca6
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-12-25 15:49:56 -0600

    styles added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {useState} from 'react';
 import boxes from './boxes';
 
-function App() {
+function App(props) {
   // console.log(boxes)
   const [squareArray, setSquareArray] = useState(boxes)
- 
-  const squareEl = squareArray.map(squares => (<div className = "box"></div>))
+  
+  const styles = {backgroundColor : props.darkMode ?  "#222222" : "#cccccc"}
+
+  const squareEl = squareArray.map(squares => (<div style={styles} className = "box" key ={squareArray.id}></div>))
   return (
     <div>
       {squareEl}
